d0565c19cae9a6181ab7f0c863e6140d4f38f5c5
weic <cw_bupt@163.com>
2017-07-14 23:39:11 +0800

    ok

--- src/newcoder/Solution_27.java
+++ src/newcoder/Solution_27.java
@@ -1,5 +1,32 @@
 package newcoder;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class Solution_27 {
+	public String PrintMinNumber(int[] numbers){
+		if(numbers==null||numbers.length==0) return "";
+		int len=numbers.length;
+		String[] str=new String[len];
+		String out="";
+		for (int i = 0; i < len; i++) {
+			str[i]=String.valueOf(numbers[i]);
+		}
+		Arrays.sort(str, new Comparator<String>() {
+
+			@Override
+			public int compare(String o1, String o2) {
+				// TODO Auto-generated method stub
+				String c1=o1+o2;
+				String c2=o2+o1;
+				return c1.compareTo(c2);
+			}
+			
+		});
+		for (int i = 0; i < str.length; i++) {
+			out=out+str[i];
+		}
+		return out;
+	}
 
 }
