3eb148d8fc97188965d4b822c5f501813d04e3a5
weic <cw_bupt@163.com>
2017-08-30 21:52:00 +0800

    输入一个递增排序的数组和一个数字S，在数组中查找两个数，是的他们的和正好是S，如果有多对数字的和等于S，输出两个数的乘积最小的。

---
+++ src/newcoder/Solution_37.java
@@ -0,0 +1,36 @@
+package newcoder;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+//输入一个递增排序的数组和一个数字S，在数组中查找两个数
+//，是的他们的和正好是S，如果有多对数字的和等于S，输出
+//两个数的乘积最小的。
+
+public class Solution_37 {
+public ArrayList<Integer> FindNumbersWithSum(int [] array,int sum) {
+	ArrayList<Integer> out=new ArrayList<>();
+	int min=sum*sum;
+	int start=0;
+	int end=array.length-1;
+	while(start<end) {
+		 int all=array[start]+array[end];
+		if(all==sum) {
+			if(min>array[start]*array[end]) {
+				min=array[start]*array[end];
+				out.clear();
+				out.add(array[start]);
+				out.add(array[end]);
+			}
+			end--;
+		}
+		else
+			if(all>sum)
+				end--;
+			else
+				start++;
+			
+	}
+        return out;
+    }
+}
